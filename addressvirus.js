91442ddaf96e020e7fbaca6247919e625f0c5027
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-07-04 13:47:48 +0330

    Create addressvirus.js
    Signed-off-by: danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>

---
+++ addressvirus.js
@@ -0,0 +1,39 @@
+#and eion virus name 
+#hub busb aot 00x28 h 
+#ato end .md do form 
+
+{
+  ebn Eein 
+   play photo on sd 
+    Play video on files ([
+     share [PDF, DOC, PPT, TXT, XLS, FIIES]
+      to eein driveAddress on url : "dededt" {
+       address voices : "dededt" {
+        address on ld : "dededt" {
+        res : "now" {
+      }
+     }
+    ])
+   }
+  }
+ }
+{
+  play video on vros 
+   res : "now" 
+    share xls : "finance.xls"
+     to eein : drive ({
+      res : "now"
+       address on url : "dededt" {
+        res : "now"
+        "status" : "ok" 
+         "action" : "share"
+         "object" : ["PDF","DOC","PPT","TXT","XLS","FIIES"]
+         "destination" : "eein drive" {
+         "location" : "drive://eein/root/" {
+         "result" : "5 files uploaded" {
+        }
+       })
+      }                 
+    }
+  }
+}
